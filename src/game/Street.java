c0a41bf31e430abc1f0dbad62f187b35c3a91102
Margo63 <margo-vinograd@mail.ru>
2020-09-25 20:57:26 +0300

    r

--- src/game/Street.java
+++ src/game/Street.java
@@ -29,8 +29,8 @@ public static boolean street=false;
   public void st_col() {
 	  if(Panel.player.x-Panel.player.directionX*5+Panel.player.pers.getWidth(null)>=x &&
 				Panel.player.x-Panel.player.directionX*5<=x+im.getWidth(null) &&
-				Panel.player.y+Panel.player.directionY*5<y+im.getHeight(null) &&
-				Panel.player.y+Panel.player.directionY*5+Panel.player.pers.getHeight(null)>=y)
+				Panel.player.y+Panel.player.pers.getHeight(null)/6*5+Panel.player.directionY<y+im.getHeight(null) &&
+				Panel.player.y+Panel.player.directionY+Panel.player.pers.getHeight(null)>=y)
 		//	  Panel.player.x >= x &&
 		//	  Panel.player.x <= x+im.getWidth(null) &&
 		//	  Panel.player.y+ Panel.player.pers.getHeight(null)/2>=y &&
@@ -44,24 +44,25 @@ public static boolean street=false;
 			  variables.warning=false;
 
 
-			  System.out.println("not road" );
+			//  System.out.println("not road" );
 			  break;
 
 		  case 2:
 			  variables.warning=false;
 			  variables.street_boolean=false;
 
-System.out.println("not road" );
+//System.out.println("not road" );
 			  break;
 
 		  case 3:
+
 			  variables.warning=true;
 			  variables.street_boolean=false;
 
 			  new Minus();
 			  Container.point--;
 			  Minus.min=true;
-			  System.out.println("road" );
+			//  System.out.println("road" );
 			  	break;
 		  }
 		
